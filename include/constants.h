eb4e42da0ed835bb8c7e983cae266dbf7379c065
enenflash <hwee.en.tang@gmail.com>
2025-06-09 14:31:17 +0800

    fixed incorrect distances and rounding

--- include/constants.h
+++ include/constants.h
@@ -1,12 +1,13 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-const int FIELD_WIDTH = 183;
-const int FIELD_LENGTH = 224;
-const int GOAL_DIST_FROM_CENTRE = 91.5;
+const int FIELD_WIDTH = 182;
+const int FIELD_LENGTH = 243;
+const float GOAL_DIST_FROM_CENTRE = 91.5;
+const int GOAL_WIDTH = 45;
 
 const int ULTRASONIC_TOLERANCE = 20;
-const int ULTRASONIC_TO_ROBOT = 9.5;
+const float ULTRASONIC_TO_ROBOT = 9.5;
 
 const int FORWARD_TOLERANCE = M_PI/10;
 
