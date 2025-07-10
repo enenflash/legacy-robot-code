faf47fbacb4b1758e908fd66d4c35af9d40574c1
enenflash <hwee.en.tang@gmail.com>
2025-07-10 15:13:50 +0800

    tidy up

--- lib/pid/pid.hpp
+++ lib/pid/pid.hpp
@@ -5,9 +5,8 @@
 
 #include "vector.hpp"
 
-
 class PID {
-public:
+    public:
     // Variable Initilisations
     const float PROPORTIONAL_CONSTANT = 2.0;
     const float INTEGRAL_CONSTANT = 0.0005;
@@ -16,20 +15,13 @@ public:
     double previousError, integral, derivitive;
     float angle, speed;
 
-
     // Calculating Speed
-   
-
     Vector moveTo(float target_x, float target_y, float x, float y, float max_speed, double dt);
 
-private:
+    private:
     double compute(double error, double dt);
     float min(float var1, float var2);
     float max(float var1, float var2);
 };
-
-
-
-
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -24,8 +24,6 @@
 DribblerMotor dribbler = DribblerMotor(DR_DIR, DR_PWM);
 
 bool check_robot_start();
-bool ball_far(float ball_magnitude);
-bool ball_in_goal_line(float ball_angle, float goal_heading);
 float find_move_angle(Vector goal_vec, float ball_angle, float ball_magnitude);
 
 Adafruit_SSD1306 display(128, 32, &Wire, -1);
@@ -50,7 +48,6 @@ float time_start = millis();
 float time_end = millis();
 
 bool in_position = false;
-float start_time, end_time;
 Vector target_vector;
 
 void setup() {
@@ -67,7 +64,6 @@ void setup() {
   pinMode(TR_PWM, OUTPUT);
   pinMode(BL_PWM, OUTPUT);
   pinMode(BR_PWM, OUTPUT);
-  pinMode(13, OUTPUT);
 
   analogWriteFrequency(TL_PWM, 20000);
   analogWriteFrequency(TR_PWM, 20000);
@@ -125,13 +121,10 @@ void loop() {
   line_sensor.update();
 
   float heading = pos_sys.get_heading();
-  Vector posv = pos_sys.get_posv();
 
   if (!robot_start) {
     robot_start = check_robot_start();
   }
-
-  start_time = micros();
 
   // angle correction
   float ball_angle = fmodf(PI + ir_sensor.get_angle() + heading, 2 * PI) - PI;
@@ -139,42 +132,31 @@ void loop() {
 
   Vector goal_vec = pos_sys.get_relative_to(Vector(0, 78.5));
 
-  // mode proto
-  // BotData self_data = {
-  //   .possession=false, .heading=heading, .pos_vector=posv, .opp_goal_vector=goal_vec, 
-  //   .ball_strength=ir_sensor.get_magnitude(), .ball_angle=ball_angle, 
-  //   .line_vector=Vector::from_heading(line_angle, line_sensor.get_distance())
-  // };
-  // shing_mode.update(self_data);
-
   // convert unit circle heading to rotation
   float rotation = goal_vec.heading() - heading - PI/2; // convert to degrees
   while (rotation > PI) rotation -= 2*PI;
   while (rotation < -PI) rotation += 2*PI;
 
-
-  BotData self_data = {
-    .possession=false, .heading=heading, .pos_vector=posv, .opp_goal_vector=goal_vec, 
-    .ball_strength=ir_sensor.get_magnitude(), .ball_angle=ball_angle, 
-    .line_vector=Vector::from_heading(line_angle, line_sensor.get_distance())
-  };
-  bt.send_data(self_data);
-  BotData other_data;
-  if (bt.receive_data())  other_data = bt.read_data();
-  else {
-    other_data = {
-      .possession = false,
-      .heading = 0,
-      .pos_vector = Vector(0, 0),
-      .opp_goal_vector = Vector(0, 0),
-      .ball_strength = 0,
-      .ball_angle = 0,
-      .line_vector = Vector(0, 0)
-    };
-    // Serial.println("no data available");
-  }
-  
-
+  // BotData self_data = {
+  //   .possession=false, .heading=heading, .pos_vector=posv, .opp_goal_vector=goal_vec, 
+  //   .ball_strength=ir_sensor.get_magnitude(), .ball_angle=ball_angle, 
+  //   .line_vector=Vector::from_heading(line_angle, line_sensor.get_distance())
+  // };
+  // bt.send_data(self_data);
+  // BotData other_data;
+  // if (bt.receive_data())  other_data = bt.read_data();
+  // else {
+  //   other_data = {
+  //     .possession = false,
+  //     .heading = 0,
+  //     .pos_vector = Vector(0, 0),
+  //     .opp_goal_vector = Vector(0, 0),
+  //     .ball_strength = 0,
+  //     .ball_angle = 0,
+  //     .line_vector = Vector(0, 0)
+  //   };
+  //   // Serial.println("no data available");
+  // }
 
   // find movement angle
   float mv_angle = 0;
@@ -194,18 +176,6 @@ void loop() {
     dribbler.run();
   }
 
-  // mode proto
-  // float rotation = shing_mode.get_rotation();
-  // float speed = shing_mode.get_speed();
-  // float mv_angle = shing_mode.get_angle();
-  // float dribbler_on = shing_mode.get_dribbler_on();
-
-  // if (!robot_start) {
-  //   speed = 0;
-  //   dribbler_on = false;
-  // }
-
-  if (angle_correction) mv_angle -= heading;
   // Serial.print(line_sensor.get_distance());
   // Serial.print(" ");
   // Serial.print(line_sensor.get_angle() * 180 / PI);
@@ -222,47 +192,34 @@ void loop() {
   // Serial.print(" ");
   // Serial.println(rotation * 180 / PI);
 
-  if (robot_start) {
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    // display.print("posv: ");
-    // display.print(other_data.pos_vector.i);
-    // display.print(" ");
-    // display.print(other_data.pos_vector.j);
-    // display.println();
-    // display.print(loop_time);
-    display.print("x: "); display.println(pos_sys.get_posv().i);
-    display.print("y: "); display.println(pos_sys.get_posv().j);
-    display.display();
-  }
+  // if (robot_start) {
+  //   display.clearDisplay();
+  //   display.setCursor(0, 0);
+  //   // display.print("posv: ");
+  //   // display.print(other_data.pos_vector.i);
+  //   // display.print(" ");
+  //   // display.print(other_data.pos_vector.j);
+  //   // display.println();
+  //   // display.print(loop_time);
+  //   display.print("x: "); display.println(pos_sys.get_posv().i);
+  //   display.print("y: "); display.println(pos_sys.get_posv().j);
+  //   display.display();
+  // }
 
   time_end=millis();
   loop_time = time_end - time_start;
-  Serial.print(other_data.pos_vector.i);
-  Serial.print(" ");
-  Serial.println(other_data.pos_vector.j);
-  // Serial.println(line_angle * 180 / PI);
-  // Serial.println(line_sensor.get_distance());
-  Serial.println(".");
-  // delay(100);
   time_start=millis();
 
-  // mode proto
-  // if (dribbler_on) dribbler.run();
-  // else dribbler.stop();
-
-  end_time = micros();
   // if (robot_start) {
   //   target_vector = linear_pid.moveTo(0, 0, pos_sys.get_posv().i, pos_sys.get_posv().j, MAX_SPEED, (end_time-start_time)/1000.0);
   //   speed = target_vector.magnitude();
   //   mv_angle = target_vector.heading();
   // }
   
-  dribbler.stop();
+  if (angle_correction) mv_angle -= heading;
   motor_ctrl.run_motors(speed, mv_angle, rotation); // run motors 50 speed, angle (radians), rotation
 
   digitalWrite(DEBUG_LED, HIGH);
-  // delay(10);
 }
 
 bool check_robot_start() {
@@ -287,14 +244,6 @@ bool check_robot_start() {
     return true;
   }
   return false;
-}
-
-bool ball_far(float ball_magnitude) {
-  return ball_magnitude < 40;
-}
-
-bool ball_in_goal_line(float ball_angle, float goal_heading) {
-  return ball_angle > goal_heading - FORWARD_TOLERANCE && ball_angle < goal_heading + FORWARD_TOLERANCE;
 }
 
 float find_move_angle(Vector goal_vec, float ball_angle, float ball_magnitude) {
