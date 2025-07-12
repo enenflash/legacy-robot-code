77bc31d44522d0bcb9744403b2cf9bf560782c46
enenflash <hwee.en.tang@gmail.com>
2025-07-12 16:28:20 +0800

    goal pos vectors

--- include/constants.h
+++ include/constants.h
@@ -1,6 +1,8 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
+#include "vector.hpp"
+
 const int FIELD_WIDTH = 182;
 const int FIELD_LENGTH = 243;
 const float GOAL_DIST_FROM_CENTRE = 91.5;
@@ -12,5 +14,8 @@ const float ULTRASONIC_TO_ROBOT = 9.5;
 const float FORWARD_TOLERANCE = M_PI/10;
 
 const float MAX_SPEED = 100;
+
+Vector opp_goal_pos_vector(0, 78.5);
+Vector own_goal_pos_vector(0, -78.5);
 
 #endif
