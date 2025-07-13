05dfaed1a9200eab5ec14af95afc6dc75764969c
enenflash <hwee.en.tang@gmail.com>
2025-07-13 19:06:06 +0800

    reverse dribbler direction

--- lib/dribbler/dribbler.cpp
+++ lib/dribbler/dribbler.cpp
@@ -7,7 +7,7 @@ DribblerMotor::DribblerMotor(int dir_pin, int pwm_pin) {
 
 void DribblerMotor::run(float speed) {
     int pwmSpeed = speed / 100 * 255;
-    digitalWrite(this->DIR_PIN, LOW);
+    digitalWrite(this->DIR_PIN, HIGH);
     analogWrite(this->PWM_PIN, pwmSpeed);
 }
 
