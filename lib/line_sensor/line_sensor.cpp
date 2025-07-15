61c71765a7e6741a53e3ad652eea73b848369d2c
Yuet Shing Feng <fengyuetshing@gmail.com>
2025-07-15 20:58:41 +0800

    robot now uses line sensor to do wireless communication

--- lib/line_sensor/line_sensor.cpp
+++ lib/line_sensor/line_sensor.cpp
@@ -1,7 +1,6 @@
 #include "line_sensor.hpp"
 
-bool LineSensor::read_serial(float* result, int num_floats) {
-    const size_t total_bytes = num_floats * sizeof(float);
+bool LineSensor::read_serial(float* result) {
     const size_t max_buffer = 256;
     uint8_t buffer[max_buffer];
     int len = 0;
@@ -13,8 +12,8 @@ bool LineSensor::read_serial(float* result, int num_floats) {
 
     // find the last 'e' that has enough bytes after it
     int start_index = -1;
-    for (int i = len - total_bytes - 1; i >= 0; i--) {
-        if (buffer[i] == 'e' && (i + 1 + total_bytes <= len)) {
+    for (int i = len - TOTAL_BYTES - 1; i >= 0; i--) {
+        if (buffer[i] == 'e' && (i + 1 + TOTAL_BYTES <= len)) {
             start_index = i;
             break;
         }
@@ -26,18 +25,24 @@ bool LineSensor::read_serial(float* result, int num_floats) {
     }
 
     // Step 3: Extract the float bytes
-    const uint8_t* float_bytes = &buffer[start_index + 1];
-    memcpy(result, float_bytes, total_bytes);
+    const uint8_t* received_bytes = &buffer[start_index + 1];
+    memcpy(result, received_bytes, TOTAL_BYTES);
 
     return true;
 }
-
+void LineSensor::send_bot_data(BotData self_data) {
+    uint8_t buffer[sizeof(BotData) + 1];
+    buffer[0] = 'd';
+    memcpy(&buffer[1], &self_data, sizeof(BotData));
+    Serial2.write(buffer, sizeof(BotData) + 1);
+}
 void LineSensor::update() {
-    float data[2];
-    this->read_success = this->read_serial(data, 2);
+    float data[TOTAL_BYTES];
+    this->read_success = this->read_serial(data);
     if (this->read_success) {
         this->angle = data[0];
         this->distance = data[1];
+        memcpy(&this->other_data, &data[2], sizeof(BotData));
     }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ MotorController motor_ctrl(0.8);
 DribblerMotor dribbler(DR_DIR, DR_PWM);
 Adafruit_SSD1306 display(128, 32, &Wire, -1);
 
-Bluetooth bluetooth_comm;
+// Bluetooth bluetooth_comm;
 
 OneRobot one_robot_mode;
 Defend defend_mode;
@@ -50,9 +50,9 @@ float time_end = millis();
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial2.begin(115200); // Line Sensor
+  Serial2.begin(921600); // Line Sensor
   Serial6.begin(115200); // IR Sensor
-  bluetooth_comm.begin();
+  // bluetooth_comm.begin();
 
   pinMode(DEBUG_LED, OUTPUT);
 
@@ -127,9 +127,10 @@ void loop() {
   };
 
   // bluetooth communication
-  bluetooth_comm.send_data(self_data);
-  BotData other_data = bluetooth_comm.read_data();
-
+  // bluetooth_comm.send_data(self_data);
+  line_sensor.send_bot_data(self_data);
+  // BotData other_data = bluetooth_comm.read_data();
+  BotData other_data = line_sensor.other_data;
   OutputData output = one_robot_mode.update(self_data, other_data, loop_time);
   // OutputData output = defend_mode.update(self_data, other_data, loop_time);
   // OutputData output = go_to_robot.update(self_data, other_data, loop_time);
@@ -152,7 +153,8 @@ void loop() {
   // print data to serial
   // print_botdata(self_data);
   // print_botdata(other_data);
-  // Serial.print(mv_angle * 180 / PI);
+  Serial.printf("received: %.2f loop_time: %d\n", line_sensor.angle, loop_time);
+
   // Serial.print(" ");
   // Serial.println(rotation * 180 / PI);
 
@@ -180,6 +182,22 @@ void loop() {
   
   // run motors
   if (angle_correction) mv_angle -= heading;
+
+  rotation = other_data.heading - heading;
+  if (rotation > PI) rotation -= 2 * PI;
+  else if (rotation < -PI) rotation += 2 * PI;
+
+  // PID movement_pid;
+  // Vector movement = movement_pid.get_movement(posv, line_sensor.other_data.pos_vector, 100, loop_time / 1000.0);
+  // Vector movement = other_data.pos_vector.relative_to(posv);
+  // speed = 30;
+  // if (movement.magnitude() < 5) {
+  //   speed = 0;
+  // } 
+
+  // float angle = movement.heading();
+  // if (angle > PI) angle -= 2 * PI;
+  // else if (angle < -PI) angle += 2 * PI;
   motor_ctrl.run_motors(speed, mv_angle, rotation);
   digitalWrite(DEBUG_LED, HIGH);
 }
